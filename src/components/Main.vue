bc7c079c4fe4e950f3ff5b4b24a7e775ff4a1fb8
FrancescoBoco <francesco.bocoux@gmail.com>
2023-07-25 20:13:24 +0200

    fine main

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -83,14 +83,170 @@ export default {
     </div>
 </div>
 <div id="wrapper-section-main-two">
-    <h3>
-        WE HELP TO DELIVER THE BUSINESS CASE 
-    </h3>
     <div>
-
+        <h3>
+            WE HELP TO DELIVER THE BUSINESS CASE 
+        </h3>
+        <p>
+            When, while lovely valley teems with vapour around meand eridian sun strikes the upper impenetrable foliage of my trees, and but a thousand unknown
+        </p>
+        <img src="../consulting-landing/btn-hover.png" alt="">
     </div>
 
 </div>
+<!-- INIZIO MAIN TRE  -->
+<div id="main-three">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+            PROJECT
+            </div>
+            <div class="col-7">
+                <h2>
+                    OUR EXPERT TRUSTED CONSULTANTS HELP CLIENTS 
+                </h2>
+            </div>
+            <span>
+                icona icona 
+            </span>
+        </div>
+        <div class="row">
+            <div class="col-4">
+                <img src="../consulting-landing/images/DRY-1-790x592.jpg" alt="">
+            </div>
+            <div class="col-4">
+                <img src="../consulting-landing/images/221bf0b7-8134-43bb-936a-5acbe42db64a-790x592.jpg" alt="">
+
+            </div>
+            <div class="col-4">
+                <img src="../consulting-landing/images/z1el4c4p-790x592.jpg" alt="">
+            </div>
+        </div>
+    </div>
+</div>
+ <!--  MAIN 4  -->
+<div id="main-four">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                PROJECT
+            </div>
+            <div class="col-7">
+
+            </div>
+            <div class="col-5">
+                <h2>
+                    WHAT'S THE CUSTOMER SAY ABOUT OUR WORK
+                </h2>
+            </div>
+        </div>
+    </div>
+</div>
+ <!-- SEZIONE MAIN 4  -->
+<div id="section-main-four">
+    <div class="my-row">
+        <div class="my-col">
+            <p>
+                When, while lovely valley teems with vapour around meand eridian sun strikes the upper impenetrable foliage of my trees, and but a thousand unknown plants are noticed when I hear the buzz of the little 
+            </p>
+            <h4>
+                Vera Duncan 
+            </h4>
+            <div>
+                amazon inc 
+            </div>
+        </div>
+        <div class="my-col">
+            <p>
+                When, while lovely valley teems with vapour around meand eridian sun strikes the upper impenetrable foliage of my trees, and but a thousand unknown plants are noticed when I hear the buzz of the little 
+            </p>
+            <h4>
+                Vera Duncan 
+            </h4>
+            <div>
+                amazon inc 
+            </div>
+        </div>
+        <div class="my-col">
+            <p>
+                When, while lovely valley teems with vapour around meand eridian sun strikes the upper impenetrable foliage of my trees, and but a thousand unknown plants are noticed when I hear the buzz of the little 
+            </p>
+            <h4>
+                Vera Duncan 
+            </h4>
+            <div>
+                amazon inc 
+            </div>
+        </div>
+    </div>
+</div>
+<!-- INIZIO MAIN CINQUE  -->
+<div id="main-five">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                BLOG
+            </div>
+            <div class="col-7">
+                <h2>
+                    THE RECENT NEWS YOU MAST READ IT 
+                </h2>
+            </div>
+            <span>
+                VIEW ALL
+            </span>
+        </div>
+        <div class="row">
+            <div class="col-6">
+                <img src="../consulting-landing/images/business-people-working-together-on-project-and-5FHSKBL-1390.jpg" alt="">
+                <div class="text-absolute">
+                   <div>
+                    <span>
+                        july 4, 2019 by paul
+                    </span>
+                    <h3>
+                        Canadian Consulting Firm acquired by UK Giant
+                    </h3>
+                    <p>
+                        When, while lovely valley teems with vapour around meand eridian sun strikes the upper impenetrable foliage of my trees, and but a ..
+                    </p>
+                   </div>
+                </div>
+            </div>
+            <div class="col-6">
+                <img src="../consulting-landing/images/startup-business-people-and-strategy-board-PAJ3P9K-1390x1042.jpg" alt="">
+                <div class="text-absolute">
+                   <div>
+                        <span>
+                            july 4, 2019 by paul
+                        </span>
+                        <h3>
+                            Canadian Consulting Firm acquired by UK Giant
+                        </h3>
+                        <p>
+                            When, while lovely valley teems with vapour around meand eridian sun strikes the upper impenetrable foliage of my trees, and but a ..
+                        </p>
+                   </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div id="main-six">
+    <div >
+        <h3>
+            LET'S TALK ABOUT WORK
+        </h3>
+        <p>
+            A wonderful serenity has taken possession Far far away, behind the word mountains.
+        </p>
+        <button>
+            GET STARTED
+        </button>
+    </div>
+</div>
+
+
 
 </template>
 
@@ -152,5 +308,169 @@ export default {
     }
 }
 // MAIN TWO SECOND SECTION 
+#wrapper-section-main-two{
+    height: 180px;
+    width: 100%;
+    background-color: #22213B;
+    > div{
+    background-color: lightgreen;
+    width: 90%;
+    height: 100%;
+    display: flex;
+    position: relative;
+        h3{
+            width: 350px;
+            padding-top: 50px;
+            margin-left: 20%;
+        }
+    }
+    p{
+        width: 350px;
+        padding-top: 40px;
+    }
+    img{
+        height: 50px;
+        position: absolute;
+        bottom: 0;
+        right: 0;
+    }
+}
+// MAIN THREE SECOND SECTION 
 
+#main-three{
+    background-color: #22213B;
+    .container{
+        .row{
+            padding-top: 50px;
+            color: white;
+            position: relative;
+            // ????????
+            span{
+            position: absolute;
+            text-align: end;
+            }
+            .col-12{
+                padding-bottom: 20px;
+            }
+        }
+       img{
+        width: 100%;
+       }
+       
+    }
+}
+// MAIN FOUR SECOND SECTION 
+
+#main-four{
+    background-color: #22213B;
+    .container{
+        padding-bottom: 30px;
+        .row{
+            padding-top: 50px;
+            color: white;
+            position: relative;
+            text-align: end;
+            // ????????
+            .col-12{
+                padding-bottom: 20px;
+
+            }
+        }
+    }
+}
+#section-main-four{
+    width: 100%;
+    background-color: #22213B;
+    display: flex;
+    justify-content: end;
+    .my-row{
+        width: 80%;
+        background-color: white;
+        padding: 50px 0 ;
+        display: flex;
+
+        .my-col{
+            width: calc(100% / 3);
+            padding:0 40px;
+        }
+    }
+}
+// MAIN FIVE SECOND SECTION 
+
+#main-five{
+    background-color: #22213B;
+    padding-bottom: 300px ;
+    padding-top: 50px;
+
+    .container{
+        .row{
+
+            padding-top: 30px;
+            color: white;
+            position: relative;
+            // ????????
+            > span{
+            position: absolute;
+            text-align: end;
+            }
+            .col-12{
+                padding-bottom: 20px;
+            }
+            img{
+                width: 100%;
+            }
+            .col-6{
+                position: relative;
+            }
+            .text-absolute{
+                height: 300px;
+                width: 400px;
+                background-color: white;
+                position: absolute;
+                bottom: -150px;
+                right: 10px;
+                div{
+                    padding: 30px;
+                    *{
+                        padding: 15px;
+                    }
+                    span{
+                    color: red;
+                    padding-bottom: 10px;
+                    }
+                    h3{
+                        color: black;
+                        font-size: 1.5em
+                    }
+                    p{
+                        color: rgb(56, 56, 56)
+                    };
+                }
+            }
+        }
+           
+    }
+}
+#main-six{
+    height: 400px;
+    background-image: url(../consulting-landing/images/business-people-working-together-on-project-and-B3MZ4TX.jpg);
+    background-size: cover;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    > div{
+        width: 400px;
+        max-height: 100%;
+        text-align: center;
+        color: white;
+        button{
+            border:none;
+            background-color:#22213B;
+            color: white;
+            padding: 15px 30px;
+            font-size: 0.9em;
+            font-weight: bold;
+        }
+    }
+}
 </style>
